1d81f861d9e4eeafeefc914a302f2affbc15b4c6
Emil-Skovbo <emilskovbo18@gmail.com>
2019-11-14 14:19:22 +0100

    joke

--- src/main/java/rest/DemoResource.java
+++ src/main/java/rest/DemoResource.java
@@ -73,23 +73,42 @@ public class DemoResource {
         String thisuser = securityContext.getUserPrincipal().getName();
         return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
     }
-    
-        @GET
+
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("person/{id}")  
-    public String getSwPeopleData(@PathParam("id") int id) throws MalformedURLException, IOException{
-    URL url = new URL("https://swapi.co/api/people/"+id);
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestMethod("GET");
-    con.setRequestProperty("Accept", "application/json;charset=UTF-8");
-    con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
-    Scanner scan = new Scanner(con.getInputStream());
-    String jsonStr = null;
-    if (scan.hasNext()) {
-      jsonStr = scan.nextLine();
-      //jsonStr += "\n";
+    @Path("person/{id}")
+    public String getSwPeopleData(@PathParam("id") int id) throws MalformedURLException, IOException {
+        URL url = new URL("https://swapi.co/api/people/" + id);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+        con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
+        Scanner scan = new Scanner(con.getInputStream());
+        String jsonStr = null;
+        if (scan.hasNext()) {
+            jsonStr = scan.nextLine();
+            //jsonStr += "\n";
+        }
+        scan.close();
+        return jsonStr;
     }
-    scan.close();
-    return jsonStr;
-  }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("joke/{id}")
+    public String getSpotifyPeopleData(@PathParam("id") int id) throws MalformedURLException, IOException {
+        URL url = new URL("https://xkcd.com/"+ id +"/info.0.json");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+        con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
+        Scanner scan = new Scanner(con.getInputStream());
+        String jsonStr = null;
+        if (scan.hasNext()) {
+            jsonStr = scan.nextLine();
+            //jsonStr += "\n";
+        }
+        scan.close();
+        return jsonStr;
+    }
 }
